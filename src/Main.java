2aae5c046ed1ca682ce3d3c3d8cf18b176da58f8
LukaszBuSDA <bulerluk@gmail.com>
2019-11-12 19:38:53 +0100

    start projektu

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
-public class Main {
+import java.time.Clock;
 
+public class Main {
+//sout
+    // ctrl + d
     public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("pierwszy program");
 
     }
 }
